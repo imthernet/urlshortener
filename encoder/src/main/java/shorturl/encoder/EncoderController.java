6a3f52110af7c54034f74ff868a51e606d75e26b
imternet <39921871+imthernet@users.noreply.github.com>
2024-04-28 10:24:41 +0200

    finish encoder

--- encoder/src/main/java/shorturl/encoder/EncoderController.java
+++ encoder/src/main/java/shorturl/encoder/EncoderController.java
@@ -1,12 +1,44 @@
 package shorturl.encoder;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.apache.commons.text.RandomStringGenerator;
+
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.util.Random;
 
 @RestController
 public class EncoderController {
     private final UrlRepository urlRepository;
+    private final Random random = new Random();
+    private final RandomStringGenerator generator = new RandomStringGenerator.Builder()
+            .withinRange('0', 'z')
+            .filteredBy(Character::isLetterOrDigit)
+            .get();
+
 
     public EncoderController(UrlRepository urlRepository) {
         this.urlRepository = urlRepository;
     }
+
+
+    @PostMapping
+    private ResponseEntity<Void> EncodeUrl(String newUrl, UriComponentsBuilder ucb) {
+        String shortId = generator.generate(8);
+        while (urlRepository.existsById(shortId)) {
+            shortId = generator.generate(8);
+        }
+        Url url = new Url(shortId,newUrl,LocalDateTime.now());
+        urlRepository.save(url);
+        URI locationOfNewUrl = ucb
+                .port(8082)
+                .path("/{shortId}")
+                .buildAndExpand(shortId)
+                .toUri();
+        return ResponseEntity.created(locationOfNewUrl).build();
+    }
+
 }
